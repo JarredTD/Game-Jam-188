0f1467e87a40e8252397a92a94d9a11771200ab4
MarkHelminiak <166643989+MarkHelminiak@users.noreply.github.com>
2025-07-04 17:07:59 -0400

    Start

--- Assets/Scripts/BoxRotate.cs
+++ Assets/Scripts/BoxRotate.cs
@@ -1,22 +1,45 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.Rendering;
 
 public class BoxRotate : MonoBehaviour
 {
-
+    public float rotateSpeed = 1;
+    public bool rotating = false;
+    private float speed = 1;
+    private float rotateTarget = 0;
     void OnAwake()
     {
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Q))
+        if (rotating)
         {
-            transform.Rotate(0, 0, 90);
+            
+            rotateTarget -= Mathf.Abs(speed * Time.deltaTime);
+            if (rotateTarget < 0) rotating = false;
+
+            transform.Rotate(0, 0, speed * Time.deltaTime);
         }
-        if (Input.GetKeyDown(KeyCode.E))
+        else
         {
-            transform.Rotate(0, 0, -90);
+            if (Input.GetKey(KeyCode.Q))
+            {
+                rotating = true;
+                speed = 1 * rotateSpeed;
+                rotateTarget = 90;
+                //transform.Rotate(0, 0, rotateSpeed * Time.deltaTime);
+            }
+            else if (Input.GetKey(KeyCode.E))
+            {
+                rotating = true;
+                speed = -1 * rotateSpeed;
+                rotateTarget = 90;
+                //transform.Rotate(0, 0, -rotateSpeed * Time.deltaTime);
+            }
         }
+
+        
     }
 }
